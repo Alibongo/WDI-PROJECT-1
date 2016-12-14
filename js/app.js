825791878ad87d0b6ce797c72ff723b94318f516
Alibongo <oioimolloy@gmail.com>
2016-12-14 09:46:59 +0000

    0940 weds edit

--- js/app.js
+++ js/app.js
@@ -7,8 +7,7 @@ var level = 1;
 
 //start button. Original error. had everything in the start function!
 // made another function PickBox to be called and trigger start. See line 22.
-function start(){
-// Scoring set to zero value to begin with.
+function start() {
   $score = $('#score span');
   $level = $('#level span');
   $highScore = $('#highScore span');
@@ -35,7 +34,7 @@ function pickBox() {
       if ($($lis[random]).attr('class') === 'zombie') {
         $score.text($score.value -= 5);
       }
-      $($lis[random]).attr('class','grave');
+      $($lis[random]).attr('class', 'grave');
     }, (1500 / (level)));
   }, 600);
 
@@ -46,7 +45,7 @@ function pickBox() {
       $(this).attr('class', 'grave');
       console.log('groan');
     } else if ($(this).attr('class') === 'human') {
-      $score.text($score.value -= 10 );
+      $score.text($score.value -= 10);
       new Audio('audio/scream.wav').play();
       $(this).attr('class', 'grave');
     } else {
@@ -70,186 +69,16 @@ function pickBox() {
       level = 4;
     } else if ($score.value >= 350 && $score.value < 450) {
       level = 5;
-    } else if ($score.value <= -20){
+    } else if ($score.value <= -20) {
 
     }
     $level.text(level);
 
-
   }
-  // Refreshing via button
-  $(document).ready(function(){
-    $('#refresh').click(function(){
+
+  $(document).ready(function() {
+    $('#refresh').click(function() {
       location.reload();
     });
   });
 }
-
-// var $human = $('li');
-//
-// setInterval(function() {
-//   if ($('.human').length < 2) {
-//       // this is picking a random index from the available squares index.
-//     var random = Math.floor(Math.random() * $availableSquares.length);
-//       // this is retrieving the value of the element at that index
-//     var randomElement  = $availableSquares[random];
-//       // this is splicing out the element at that index so that it can't be selected again
-//     $availableSquares.splice(random, 1);
-//       // this is now using that randomly selected element from the available squares array.
-//     $($human[randomElement]).attr('class', 'human');
-//   }
-// }, 500);
-//
-//
-//
-// $human.on('click', function() {
-//   if ($(this).attr('class') === 'human') {
-//     $score.text($score.value -= 10);
-//     $(this).attr('class', 'grave');
-//   } else {
-//     $score.text($score.value -= 0);
-//   }
-//   // if ($score.value === 50)
-//   //   alert('game Over');
-// });
-
-
-
-// refreshing the page automatically
-// setTimeout(function() {
-//   // location.reload();
-// }, 6000);
-
-
-
-
-//
-// // function checkForWin($score.value){
-//   console.log($score.value);
-// for (var i = 0; i< winningCombinations.length;)
-//
-// }
-
-
-
-// function checkForWin(movesArray, player){
-//   console.log(movesArray,player);
-//   // we're incrementing on the winningCominbation array here:
-//   for (var i = 0; i< winningCombinations.length; i++){
-//     winCounter = 0;
-//
-//     for (var j = 0; j < winningCombinations[i].length; j++) {
-//
-//       // indexof searches through the array
-//       if(movesArray.indexOf(winningCombinations [i][j]) !== -1){
-//         winCounter++;
-//       }
-//       if (winCounter ===3){
-//         alert('Game Over,' + player + ' Wins!');
-//       }
-//     }
-//   }
-// }
-
-
-
-
-
-
-
-// how do I make the square  available again - pushing it back into the array of available squares.
-// Index of  - push the index of the newly available squares in the lis array into the available squares array.
-
-
-
-
-
-
-// $human.on('click', function()){
-//   if
-// }
-
-// var $zombieClick = $('li'); {
-//   $zombieClick.on =('click', function(){
-//     var $this = $(this);
-//     toggleClass
-// //     console.log(this.fileName);
-//
-//     var audio = new Audio('../audio/'+ $this.attr('id') + '.mp3');
-//     audio.play();
-//   };
-// }
-//
-//
-//
-//
-
-
-
-
-
-
-
-
-
-
-//
-// $('li').on('click', function(){
-//   var $element = $(this);
-//   // swing refers to one of the default easing options
-//   $element.fadeOut('slow','swing', function(){
-//     setTimeout(function(){
-//       $element.fadeIn();
-//     },1000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//
-// $(function(){
-// // ** Global Variables
-//   // var $score = '';
-//   // var $lives = '';
-//   //
-// var grave = '';
-//   // var grid = '';
-//
-//
-//   // Welcoming message  - start?
-//   // player hits start
-//   // first zombie appears
-//   // player hits zombie
-//   // player misses zombie
-//   //noise triggered when zombie is hit
-//   // zombie wobbles
-//   // when hit, triggers score update
-//   // when missed triggers loss of life
-//   // loss of life triggers message and life update
-//   //
-// });
-//
-//
-//
-// var $squares= $ flash (element){
-//   element.addClass('red');
-//   element.fadeOut('swing', function(){
-//     setTimeout(function(){
-//       element.fadeIn();
-//     },1000);
-//   });
-// }
-// ]
-//
-//   $('li').on('click', function(){
-//     $('.grid').append($(this));
-//   });
